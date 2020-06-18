7b527c900965d910d06edf50f0f4c1b87ec81a9a
Cristina Laryea <crispad06@gmail.com>
2020-06-17 22:13:48 -0400

    added footer

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import Footer from './Components/Footer';
+import NavBar from './Components/NavBar';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
-  <h1>Landing</h1>
+      <h1>Landing</h1>
+      <Footer />
     </div>
   );
 }
