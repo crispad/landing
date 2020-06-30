51adeb55d481b493fe918a405226d325f9347f5e
Cristina Laryea <crispad06@gmail.com>
2020-06-29 21:26:27 -0400

    added semantic ui react form to landing page

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Components/Footer';
 import content_team from './Images/content_team.png';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 import NavBar from './Components/NavBar';
 import './App.css';
 
@@ -9,6 +10,20 @@ function App() {
     <div className="App">
       <h1>Landing</h1>
       <img src={content_team} alt="content_team" className="content_team" />
+      <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='First Name' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Last Name' />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
       <Footer />
     </div>
   );
