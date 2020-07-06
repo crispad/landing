6555dc86b3b8d128c9ce3567ec490b1e65a938c4
Cristina Laryea <crispad06@gmail.com>
2020-07-06 17:51:21 -0400

    form styling

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import Footer from './Components/Footer';
-import content_team from './Images/content_team.png';
-import { Button, Form } from 'semantic-ui-react'
-import NavBar from './Components/NavBar';
-import './App.css';
+import React from "react";
+import Footer from "./Components/Footer";
+import content_team from "./Images/content_team.png";
+import { Button, Form } from "semantic-ui-react";
+import NavBar from "./Components/NavBar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Landing</h1>
       <img src={content_team} alt="content_team" className="content_team" />
-      <Form>
-    <Form.Field>
-      <label>First Name</label>
-      <input placeholder='First Name' />
-    </Form.Field>
-    <Form.Field>
-      <label>Last Name</label>
-      <input placeholder='Last Name' />
-    </Form.Field>
-    {/* <Form.Field>
-      <Checkbox label='I agree to the Terms and Conditions' />
-    </Form.Field> */}
-    <Button type='submit'>Submit</Button>
-  </Form>
+      <div className="Form">
+        <Form>
+          <Form.Field>
+            <div className="firstname">
+              <label>First Name</label>
+              <input placeholder="First Name" />
+            </div>
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name</label>
+            <input placeholder="Last Name" />
+          </Form.Field>
+          <div className="button">
+            <Button type="submit">Submit</Button>
+          </div>
+        </Form>
+      </div>
       <Footer />
     </div>
   );
